274cb0c3514bd84adfa56a180de23af3cb6106ae
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-05 15:45:02 -0500

    SQL script that writes a table titled album containing descriptive columns and a primary key to ensure all entries are unique.

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT NOT NULL, 
+    artist VARCHAR(100) NOT NULL,
+    record_name VARCHAR(100) NOT NULL,
+    release_date DATE NOT NULL,
+    sales INT UNSIGNED NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+
+);
