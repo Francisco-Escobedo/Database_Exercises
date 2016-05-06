136162049ede3d7b4cdc4cb1e7a699b9846b9016
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-06 10:08:29 -0500

    Refactored script to use a single INSERT statement for all records.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,6 +1,5 @@
 USE codeup_test_db;
 
 INSERT INTO albums (artist, record_name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop');
-INSERT INTO albums (artist, record_name, release_date, sales, genre)
-VALUES ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock');
+VALUES  ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop'),
+        ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock');
