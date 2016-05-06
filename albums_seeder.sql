7ef23aa1f470f65fc1963d9d40eb938ef98fc819
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-06 09:52:43 -0500

    Added values to be inserted to album table.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,4 +1,6 @@
 USE codeup_test_db;
 
 INSERT INTO albums (artist, record_name, release_date, sales, genre)
-VALUES ()
+VALUES ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop');
+INSERT INTO albums (artist, record_name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock');
