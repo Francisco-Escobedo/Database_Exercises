2ac239a46ce3d7933780975ca6b3dcfbd5571a90
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-06 13:20:03 -0500

    Changed release date update to 1901 (which is the earliest limit for the YEAR parameter in SQL).

--- update_exercises.sql
+++ update_exercises.sql
@@ -8,7 +8,7 @@ SELECT * FROM albums;
 SELECT '1980' AS 'Albums released before';
 SELECT record_name FROM albums WHERE release_date<1980;
 UPDATE albums
-SET release_date = 1970
+SET release_date = 1901
 WHERE release_date<1980;
 SELECT * FROM albums WHERE release_date = 1805;
 SELECT 'Michael Jackson' AS 'Albums by';
