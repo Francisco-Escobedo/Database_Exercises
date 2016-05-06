7964e0f45abcbbb85dfe72425722717de0e4c1d4
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-06 15:48:57 -0500

    Created queries for employee first name, employee last name, those hired in the 90s

---
+++ where_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT emp_no, first_name, last_name, hire_date
+FROM employees
+WHERE hire_date > '1990-01-01';
