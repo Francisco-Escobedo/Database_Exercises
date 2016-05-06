45ea6ae4ee6dbe977ce6cffb9d59cf9806f25248
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-06 15:53:18 -0500

    Added queries for employees born on Christmas and that have a q in their last name

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,3 +11,11 @@ WHERE last_name LIKE 'E%';
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
 WHERE hire_date > '1990-01-01';
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
