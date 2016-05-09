91282d371622532a516c32d33b0f8e5ed5a43f85
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 11:17:43 -0500

    Inserted query that searches for employees whose last names start and end with E that are unique

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,3 +4,8 @@ SELECT title
 FROM titles
 GROUP BY title ASC;
 
+SELECT DISTINCT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY last_name;
