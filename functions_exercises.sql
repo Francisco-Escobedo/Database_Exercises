3b40922c020759d76647bd2188a397d728d04fee
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 13:30:16 -0500

    Added updated version of query for 'Irena', 'Vidya', or 'Maya' which uses count and group by to display the number of employees for each geder with those names

---
+++ functions_exercises.sql
@@ -0,0 +1,46 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name, gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') 
+AND gender = 'M'
+COUNT(*)
+AND gender = 'F'
+COUNT(*)
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name IN ('IRENA', 'Vidya', "Maya")
+GROUP BY gender;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+
+SELECT emp_no, first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1989-12-31' AND '2000-01-01';
+
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1989-12-31' AND '2000-01-01'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
