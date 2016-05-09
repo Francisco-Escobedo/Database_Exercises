2db2c978ae9b2ccfa5b9ca29b52487c96cded0e4
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 11:25:27 -0500

    Added query to find last names with a q but not a qu.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -8,4 +8,10 @@ SELECT DISTINCT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'
-GROUP BY first_name AND last_name;
+GROUP BY first_name, last_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name ;
