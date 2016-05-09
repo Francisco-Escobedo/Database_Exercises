9d602db1ddb66719a02f5f4646f8e0305a4140a3
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 11:45:16 -0500

    Query to find employees born on Christmas and hired in the 90s limited to the first 5 employees

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,11 @@ SELECT last_name
 FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1989-12-31' AND '2000-01-01'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
