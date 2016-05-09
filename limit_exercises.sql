18b0f1642bf7266bf32c0e93f213562d83bd8809
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 11:47:08 -0500

    Updated previous query (selecting employees born on christmas and hired in the 90s) to show the 10th batch of results

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -12,3 +12,10 @@ AND birth_date LIKE '%-12-25'
 ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5;
 
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1989-12-31' AND '2000-01-01'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
+
