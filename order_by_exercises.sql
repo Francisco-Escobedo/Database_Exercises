ba96df47a4bbb3e0b747421a9c34410feb2c9490
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 10:40:30 -0500

    Reversed sort order for the first and second queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,13 +4,13 @@ SELECT emp_no, first_name, last_name, gender
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya') 
     AND gender = 'M'
-    ORDER BY last_name ASC, first_name ASC;
+    ORDER BY last_name DESC, first_name DESC;
 
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 OR last_name LIKE '%E'
-ORDER BY emp_no ASC;
+ORDER BY emp_no DESC;
 
 SELECT emp_no, first_name, last_name
 FROM employees
