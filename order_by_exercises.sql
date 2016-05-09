c069b884a625bee59a94ddfe74b3060f03a02991
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-09 10:44:34 -0500

    Changed the last query for employees hired in the 90s and born on christmas that first reqult is the oldest and was hired last.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -19,12 +19,13 @@ AND last_name LIKE '%E';
 
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
-WHERE hire_date > '1989-12-31';
+WHERE hire_date BETWEEN '1989-12-31' AND '2000-01-01';
 
 SELECT emp_no, first_name, last_name, hire_date, birth_date
 FROM employees
-WHERE hire_date > '1989-12-31'
-AND birth_date LIKE '%-12-25';
+WHERE hire_date BETWEEN '1989-12-31' AND '2000-01-01'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 
 SELECT emp_no, first_name, last_name, birth_date
